c274a58311553a422bcb5c47cb2cdeb248d8fdbf
fraol0912 <fraol0912@gmail.com>
2021-06-20 17:22:20 +0300

    edited 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Button from '../components/Button';
 import Layout from '../components/layout/Layout';
+import { Link } from 'gatsby';
 
 const NotFound = () => (
   <Layout>
-    <h1>Not Found</h1>
-    <p>You just hit a page that doesn&#39;t exist...</p>
+    <div className="container mx-auto px-8 text-center lg:text-left">
+      <h1 className="text-3xl lg:text-5xl font-semibold text-center my-5">Not Found</h1>
+      <p className="text-center">You just hit a page that doesn't exist...</p>
+      <div
+        className="container mx-auto my-5"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Link href="/">
+          <Button className="text-center">Go Back To Home</Button>
+        </Link>
+      </div>
+    </div>
   </Layout>
 );
 
