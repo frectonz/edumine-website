56f329dab8a0979ea62a2466f531c55e572d988c
fraol0912 <fraol0912@gmail.com>
2021-06-29 06:07:26 +0300

    head error

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,16 +3,15 @@ import Footer from './Footer';
 import Header from './Header';
 
 import { Seo } from './Seo';
-import { HeadProvider } from 'react-head';
 
 const Layout = ({ children }) => {
   return (
-    <HeadProvider>
+    <>
       <Seo />
       <Header />
       <main className="text-gray-900">{children}</main>
       <Footer />
-    </HeadProvider>
+    </>
   );
 };
 
