3e230adbddfd6c977fa2e29ebb672cce91f5a651
fraol0912 <fraol0912@gmail.com>
2021-11-06 10:38:58 +0300

    don't show header and footer on 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,22 +3,19 @@ import { Link } from "gatsby";
 
 // Components
 import Button from "../components/Button";
-import Layout from "../components/layout/Layout";
 
 const NotFound = () => (
-  <Layout>
-    <div className="container mx-auto px-8 text-center lg:text-left">
-      <h1 className="text-3xl lg:text-5xl font-semibold text-center my-5">
-        Not Found
-      </h1>
-      <p className="text-center">You just hit a page that doesn't exist...</p>
-      <div className="container flex justify-center mx-auto my-5">
-        <Link to="/">
-          <Button className="text-center">Go Back To Home</Button>
-        </Link>
-      </div>
+  <div className="container my-10 mx-auto px-8 text-center lg:text-left">
+    <h1 className="text-3xl lg:text-5xl font-semibold text-center my-5">
+      Not Found
+    </h1>
+    <p className="text-center">You just hit a page that doesn't exist...</p>
+    <div className="container flex justify-center mx-auto my-5">
+      <Link to="/">
+        <Button className="text-center">Go Back To Home</Button>
+      </Link>
     </div>
-  </Layout>
+  </div>
 );
 
 export default NotFound;
