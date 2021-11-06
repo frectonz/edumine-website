cc2814041de8551be82d1f4cd763433ad3916ece
fraol0912 <fraol0912@gmail.com>
2021-11-06 10:31:48 +0300

    replace manual styling with tailwind class

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Button from '../components/Button';
-import Layout from '../components/layout/Layout';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
+
+// Components
+import Button from "../components/Button";
+import Layout from "../components/layout/Layout";
 
 const NotFound = () => (
   <Layout>
     <div className="container mx-auto px-8 text-center lg:text-left">
-      <h1 className="text-3xl lg:text-5xl font-semibold text-center my-5">Not Found</h1>
+      <h1 className="text-3xl lg:text-5xl font-semibold text-center my-5">
+        Not Found
+      </h1>
       <p className="text-center">You just hit a page that doesn't exist...</p>
-      <div
-        className="container mx-auto my-5"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
+      <div className="container flex justify-center mx-auto my-5">
         <Link to="/">
           <Button className="text-center">Go Back To Home</Button>
         </Link>
